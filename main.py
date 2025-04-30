bc440288d9e6400ede525ae4d45194b64122ea97
livelyvoid <livelyvoid@yahoo.com>
2025-04-30 23:41:44 +0000

    Made changes

--- main.py
+++ main.py
@@ -3,22 +3,6 @@ from discord.ext import commands, tasks
 import os
 import random
 from datetime import datetime, timedelta, time as dtime
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is running"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -408,5 +392,4 @@ __Info & Monitoring:__
 """
     await ctx.send(help_text)
 
-keep_alive()
 bot.run(os.getenv('TOKEN'))
